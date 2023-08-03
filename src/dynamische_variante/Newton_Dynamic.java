0f27e08c4360b83ae99bf97c093d32c76fd18934
Ahmet_Toy <ahmet.toy@tu-darmstadt.de>
2023-08-03 13:55:01 +0200

    doc

--- src/dynamische_variante/Newton_Dynamic.java
+++ src/dynamische_variante/Newton_Dynamic.java
@@ -7,6 +7,9 @@ import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
 
+/**
+ * TODO: Implementierung von Produktregel, Kettenregel und Quotientenregel
+ */
 public class Newton_Dynamic {
 
   static final Scanner scanner = new Scanner(System.in);
